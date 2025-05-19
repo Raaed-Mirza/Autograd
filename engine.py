7f9ce560fd26bc96887e83b323dfad7d22f5767d
Raaed-Mirza <raaedmirza@gmail.com>
2025-05-19 14:53:38 -0400

    created a multiply function

--- engine.py
+++ engine.py
@@ -1,7 +1,8 @@
 class Value:
 
-    def __init__(self, data):
+    def __init__(self, data, _children=()): #_childern is a tuple
         self.data = data
+        self.prev = set(_children)
 
     def __repr__(self):
         return f"Value({self.data})"
@@ -9,9 +10,15 @@ class Value:
     def __add__(self, other):
         out = Value(self.data + other.data)
         return out
+
+    def __mul__(self, other):
+        out = Value(self.data * other.data)
+        return out
     
 a = Value(2.0)
 # print(a)
-b = Value(3.0)
+b = Value(-3.0)
+c = Value(10)
 
-print(a.__add__(b))
+print((a.__mul__(b)).__add__(c))
+print((a * b) + c)

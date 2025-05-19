cad4ec854d000c5660c616d64b7790cff2fc43e1
Raaed-Mirza <raaedmirza@gmail.com>
2025-05-19 15:06:37 -0400

    implemented prev and op to see what previous operations and datas were used before the current node

--- engine.py
+++ engine.py
@@ -1,24 +1,27 @@
 class Value:
 
-    def __init__(self, data, _children=()): #_childern is a tuple
+    def __init__(self, data, _children=(), _op=''): #_childern is a tuple used to store the inputs (or parents) of the current node
         self.data = data
-        self.prev = set(_children)
+        self.prev = set(_children) #prev is a set used to store the inputs (or parents) of the current node
+        self._op = _op #op is used to store the operation that produced this node
 
     def __repr__(self):
         return f"Value({self.data})"
     
     def __add__(self, other):
-        out = Value(self.data + other.data)
+        out = Value(self.data + other.data, (self, other), '+')
         return out
 
     def __mul__(self, other):
-        out = Value(self.data * other.data)
+        out = Value(self.data * other.data, (self, other), '*')
         return out
     
 a = Value(2.0)
 # print(a)
 b = Value(-3.0)
 c = Value(10)
+d = (a * b) + c
 
-print((a.__mul__(b)).__add__(c))
-print((a * b) + c)
+#print((a.__mul__(b)).__add__(c))
+print("d =", d)
+print("d.prev =", d.prev, "d._op = ", d._op)

64ad3f3ed68c246183b884ff8f7cdac87e37861f
Raaed-Mirza <raaedmirza@gmail.com>
2025-05-20 10:00:05 -0400

    Added other operations

--- operations.py
+++ operations.py
@@ -6,3 +6,16 @@ class Operations(Value):
 
     def __mul__(self, other):
         return Operations(self.data * other.data, (self, other), '*')
+
+    def __sub__(self, other):
+        return Operations(self.data - other.data, (self, other), '-')   
+
+    def __truediv__(self, other):   
+        return Operations(self.data / other.data, (self, other), '/')
+
+    def __pow__(self, other):
+        return Operations(self.data ** other.data, (self, other), '^')
+
+    def __neg__(self):
+        return Operations(-self.data, (self,), 'neg')
+        

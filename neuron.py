dd454a59474ec9a2e7ac706e5909ece3f5a230cf
Raaed-Mirza <raaedmirza@gmail.com>
2025-05-27 14:21:05 -0400

    Added MLP class

--- neuron.py
+++ neuron.py
@@ -23,4 +23,14 @@ class Layer:
         act = sum((wi*xi for wi, xi in zip(self.w, x)), self.b) # zip the weights and inputs together and compute the activation
         out = act.tanh() # apply the tanh activation function
         return out
-    
+
+class MLP:
+
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(sz)-1)] # create a list of layers with the specified sizes
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
